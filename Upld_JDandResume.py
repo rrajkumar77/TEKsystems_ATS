decb721237ae00d1302155588be45233151b06df
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2025-12-15 12:45:29 +0530

    Update Upld_JDandResume.py

--- Upld_JDandResume.py
+++ Upld_JDandResume.py
@@ -12,7 +12,7 @@ os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input_jd, resume_content, prompt, additional_input=""):
-    model = genai.GenerativeModel('gemini-2.5-flash')
+    model = genai.GenerativeModel('gemini-2.0-flash')
     if additional_input:
         response = model.generate_content([input_jd, resume_content, prompt, additional_input])
     else:
@@ -339,6 +339,7 @@ elif submit_general_query:
                 st.write("Please upload a resume file or enter a Job Description to proceed.")
     else:
         st.write("Please upload a resume file or enter a Job Description to proceed.")
+
 
 
 
